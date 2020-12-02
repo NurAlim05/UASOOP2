7c0d546c8392e291d9cbae1986a4ae0553a0e856
maurizaf10 <maurizaf10@gmail.com>
2020-12-02 22:57:58 +0700

    add dependencies and create table

---
+++ app/src/main/java/com/phb/crudapp/ListPonpesActivity.kt
@@ -0,0 +1,35 @@
+package com.phb.appcrud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.phb.crudapp.R
+import kotlinx.android.synthetic.main.activity_list_ponpes.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.select
+
+class ListPonpesActivity : AppCompatActivity() {
+
+    private lateinit var adapter: RVAdapterPonpes
+    private var pendidikan = ArrayList<Ponpes>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_ponpes)
+
+        adapter = RVAdapterPonpes(this, pendidikan)
+        recylerViewPonpes.adapter = adapter
+
+        getData()
+        recylerViewPonpes.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun getData() {
+        database.use {
+            pendidikan.clear()
+            var result = select(Ponpes.TABLE_PONPES)
+            var dataPonpes = result.parseList(classParser<Ponpes>())
+            pendidikan.addAll(dataPonpes)
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
--- app/src/main/java/com/phb/crudapp/MainActivity.kt
+++ app/src/main/java/com/phb/crudapp/MainActivity.kt
@@ -1,11 +1,49 @@
-package com.phb.crudapp
+package com.phb.appcrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.phb.crudapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSimpan.setOnClickListener {
+            addDataSantri()
+            addDataPonpes()
+        }
+
+        buttonLihatData.setOnClickListener {
+            toast("Data Di Lihat")
+        }
+
+        buttonUpdate.setOnClickListener {
+            toast("Data Diupdate")
+        }
+    }
+
+    private fun addDataSantri() {
+        database.use {
+            insert(Santri.TABLE_SANTRI,
+                Santri.NAMA to editTextNama.text.toString(),
+                Santri.ALAMAT to editTextAlamat.text.toString(),
+                Santri.HANDPHONE to editTextHandphone.text.toString()
+            )
+            toast("Data berhasil disimpan!")
+        }
+    }
+    private fun addDataPonpes() {
+        database.use {
+            insert(Ponpes.TABLE_PONPES,
+                Ponpes.PESANTREN to editTextPesantren.text.toString(),
+                Ponpes.ALAMATPONPES to editTextAlamatPesantren.text.toString(),
+                Ponpes.PENGASUH to editTextPengasuh.text.toString()
+            )
+            toast("Data berhasil disimpan!")
+        }
     }
 }
